c494515b844a2a0cb95d12448f6d9b789e929bb4
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2026-04-09 18:04:45 -0500

    feat: implement landing page layout and add responsive styling for core components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,10 @@
-import Footer from "@/components/footer/Footer";
+import Footer from "@/components/Footer";
 import About from "../../container/About Us/About";
 import Hero from "../../container/Hero/Hero";
 import Solution from "../../container/Solution Us/Solution";
 import Contact from "../../container/contact/Contact";
 import Portfolio from "../../container/portfolio/Portfolio";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 export default function Home() {
   return (
@@ -16,7 +15,7 @@ export default function Home() {
         <Solution/>
         <Portfolio/>
         <Contact/>
-        <Footer />
+        <Footer/>
       </div>
       <div className="w-full h-[5vh] bg-blue-950 text-white text-center pt-1">
         <p>&copy; 2024 Innovizecem</p>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import imageFooter from "../../../public/assets/Logo_color.svg";
+import imageFooter from "../../public/assets/Logo_color.svg";
 import {
   InstagramLogoIcon,
   LinkedinLogoIcon,
